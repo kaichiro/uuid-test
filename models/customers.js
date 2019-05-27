6089527493a99bd39b8ad4149af19c6e65b8d384
kaichiro <kcfg23@gmail.com>
2019-05-26 22:00:09 -0300

    Refactoring - add logging statement and fields to insert/update and add findByName function

--- models/customers.js
+++ models/customers.js
@@ -1,10 +1,19 @@
 const loggingSqlCommand = require('../utils/log')
 
+const dataFields_InsertUpdate = (data) => {
+    return `name = '${data.name}'`
+        .concat((data.birth_day) ? `, birth_day = '${data.birth_day}'` : ``)
+}
+
+const dataFields_Selects = () => {
+    return `BIN_TO_UUID(id) id, name, birth_day`
+}
+
 const findAll = connection => {
     return new Promise((resolve, reject) => {
-        const sql = `select BIN_TO_UUID(id) id, name from customers order by name;`
+        const sql = `select ${dataFields_Selects()} from customers order by name;`
         if ('true' === process.env.SHOW_SQL_CMD) {
-            loggingSqlCommand(sql)
+            loggingSqlCommand(sql, 'findAll')
         }
         connection.query(sql, (err, results) => {
             if (err) {
@@ -18,9 +27,9 @@ const findAll = connection => {
 
 const findById = (connection, id) => {
     return new Promise((resolve, reject) => {
-        const sql = `SELECT BIN_TO_UUID(id) id, name FROM customers where id = UUID_TO_BIN('${id}'); `
+        const sql = `SELECT ${dataFields_Selects()} FROM customers where id = UUID_TO_BIN('${id}');`
         if ('true' === process.env.SHOW_SQL_CMD) {
-            loggingSqlCommand(sql)
+            loggingSqlCommand(sql, 'findById')
         }
         connection.query(sql, (err, results) => {
             if (err) {
@@ -36,11 +45,27 @@ const findById = (connection, id) => {
     })
 }
 
+const findLikeName = (connection, name) => {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT ${dataFields_Selects()} FROM customers where name like '${name}%' order by name;`
+        if ('true' === process.env.SHOW_SQL_CMD) {
+            loggingSqlCommand(sql, 'findLikeName')
+        }
+        connection.query(sql, (err, results) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(results)
+            }
+        })
+    })
+}
+
 const create = (connection, data) => {
     return new Promise((resolve, reject) => {
-        const sql = `insert into customers set id = UUID_TO_BIN('${data.id}'), name = '${data.name}';`
+        const sql = `insert into customers set id = UUID_TO_BIN('${data.id}'), ${dataFields_InsertUpdate(data)};`
         if ('true' === process.env.SHOW_SQL_CMD) {
-            loggingSqlCommand(sql)
+            loggingSqlCommand(sql, 'create')
         }
         connection.query(sql, err => {
             if (err) {
@@ -54,9 +79,9 @@ const create = (connection, data) => {
 
 const update = (connection, id, data) => {
     return new Promise((resolve, reject) => {
-        const sql = `update customers set name = '${data.name}' where id = UUID_TO_BIN('${id}');`
+        const sql = `update customers set ${dataFields_InsertUpdate(data)} where id = UUID_TO_BIN('${id}');`
         if ('true' === process.env.SHOW_SQL_CMD) {
-            loggingSqlCommand(sql)
+            loggingSqlCommand(sql, 'update')
         }
         connection.query(sql, err => {
             if (err) {
@@ -72,7 +97,7 @@ const deleteOne = (connection, id) => {
     return new Promise((resolve, reject) => {
         const sql = `delete from customers where id = uuid_to_bin('${id}') limit 1;`
         if ('true' === process.env.SHOW_SQL_CMD) {
-            loggingSqlCommand(sql)
+            loggingSqlCommand(sql, 'delete')
         }
         connection.query(sql, err => {
             if (err) {
@@ -87,6 +112,7 @@ const deleteOne = (connection, id) => {
 module.exports = {
     findAll,
     findById,
+    findLikeName,
     create,
     update,
     deleteOne,
